17a30a9c6a913731c5cdc585dff0e394bf4f3cdb
dgaeck <dgaeck@myfairpoint.net>
2016-01-29 23:55:03 -0500

    just a test commit

--- tutorial/src/tutorial/First.java
+++ tutorial/src/tutorial/First.java
@@ -4,6 +4,6 @@ public class First
 {   
     public static void main(String[] args)
     {
-        System.out.println("Hello this is Java");  
+        System.out.println("Hello this is Java world");  
     }
 }
