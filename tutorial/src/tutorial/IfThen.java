536661ea0483d111b8ea87723bfb59c35e0a36fb
dgaeck <dgaeck@myfairpoint.net>
2016-12-19 22:39:33 -0500

    commit for if then example

---
+++ tutorial/src/tutorial/IfThen.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+public class IfThen {
+    public static void main(String[] args) {
+        int testscore = 80;
+       
+        if (testscore <= 79){
+        System.out.println("Less than 80");
+        }/*else {
+        	System.out.println("Greater than 80");
+        }*/
+    }
+}
