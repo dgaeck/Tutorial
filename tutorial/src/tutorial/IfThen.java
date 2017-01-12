b289706466f91cb31a87282bc138de90d22050a5
dgaeck <dgaeck@myfairpoint.net>
2017-01-11 20:03:16 -0500

    test 1/11

--- tutorial/src/tutorial/IfThen.java
+++ tutorial/src/tutorial/IfThen.java
@@ -2,7 +2,7 @@ package tutorial;
 
 public class IfThen {
     public static void main(String[] args) {
-        int testscore = 98;
+        int testscore = 90;
        
         if (testscore <= 79){
         System.out.println("Less than 75");
