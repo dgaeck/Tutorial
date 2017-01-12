9ce60a5c67d76930b34286253b4f1940bd63e946
dgaeck <dgaeck@myfairpoint.net>
2017-01-11 19:51:08 -0500

    approved
    Signed-off-by: dgaeck <dgaeck@myfairpoint.net>

--- tutorial/src/tutorial/IfThen.java
+++ tutorial/src/tutorial/IfThen.java
@@ -2,12 +2,12 @@ package tutorial;
 
 public class IfThen {
     public static void main(String[] args) {
-        int testscore = 80;
+        int testscore = 98;
        
         if (testscore <= 79){
-        System.out.println("Less than 80");
-        }/*else {
+        System.out.println("Less than 75");
+        }else {
         	System.out.println("Greater than 80");
-        }*/
+        }
     }
 }
