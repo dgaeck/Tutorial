393af097f7560805ef045382c4bb1dc6a2495e57
dgaeck <dgaeck@myfairpoint.net>
2019-02-18 22:35:59 -0500

    fix typo in print and test commit for new ide

--- tutorial/src/tutorial/IfThen.java
+++ tutorial/src/tutorial/IfThen.java
@@ -2,7 +2,7 @@ package tutorial;
 
 public class IfThen {
     public static void main(String[] args) {
-        int testscore = 90;
+        int testscore = 4;
        
         if (testscore <= 79){
         System.out.println("Less than 75");
